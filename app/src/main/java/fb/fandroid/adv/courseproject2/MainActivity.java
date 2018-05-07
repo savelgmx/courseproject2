3a4fba31ed5b5145fc5d408d5e3d80b91ab2275e
savelgmx <savel@gmx.com>
2018-05-07 16:02:27 +0700

    Класс для основного экрана и меню сделан через фрагмент Добавен класс MainFragment Класс SingleFragmentActivity

--- app/src/main/java/fb/fandroid/adv/courseproject2/MainActivity.java
+++ app/src/main/java/fb/fandroid/adv/courseproject2/MainActivity.java
@@ -1,12 +1,6 @@
 package fb.fandroid.adv.courseproject2;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.support.v4.app.Fragment;
 
 /*
 Задание 1.
@@ -37,60 +31,10 @@ import android.widget.Toast;
 При нажатии на пункт Выход и при нажатии назад при пустом бэкстеке фрагментов - приложение закрывается.
  */
 
-public class MainActivity extends AppCompatActivity {
-
-    private void showMessage(String string) {
-        Toast.makeText(this, string, Toast.LENGTH_LONG).show();
-    }
+public class MainActivity extends SingleFragmentActivity {
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-       /*Добавляем метод, который берёт данные из ресурсов меню
-       и преобразует их в пункты меню на экране.
-       По английски "inflate" переводится как надувать,
-       т.е. по замыслу разработчиков Android, мы как бы надуваем данными объект, например, меню.
-       Но на самом деле слово "inflate" происходит от словосочетания in flat - в квартиру.
-       Существует старинная традиция запускать в квартиру первым кота,
-       который исследует все закоулки дома и заявляет о своём согласии жить в нём.
-       Так и мы запускаем данные из XML-файла в объект MenuInflater.
-        */
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-
-    //Для обработки нажатий пунктов меню служит другой метод onOptionsItemSelected()
-     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-         // получим идентификатор выбранного пункта меню
-         int id = item.getItemId();
-
-         // Операции для выбранного пункта меню
-         switch (id) {
-             case R.id.action_settings:
-                 showMessage("Вы выбрали пункт Настройки");
-
-                 return true;
-             case R.id.action_search:
-                 showMessage("Вы выбрали Поиск");
-
-                 Uri uri = Uri.parse("http://google.com/search?q=haruhi");
-                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                 startActivity(intent);
-
-                 return true;
-             case R.id.action_exit:
-                 showMessage("Вы выбрали Выход");
-                 return true;
-             default:
-                 return super.onOptionsItemSelected(item);
-         }
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+    protected Fragment getFragment() {
+        return MainFragment.newInstance();
     }
 }
---
+++ app/src/main/java/fb/fandroid/adv/courseproject2/MainFragment.java
@@ -0,0 +1,89 @@
+package fb.fandroid.adv.courseproject2;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+/**
+ * Created by Administrator on 07.05.2018.
+ */
+
+public class MainFragment extends Fragment {
+
+    public static MainFragment newInstance() {
+        Bundle args = new Bundle();
+
+        MainFragment fragment = new MainFragment();
+        fragment.setArguments(args);
+        return fragment;
+
+    }
+
+    private void showMessage(String string) {
+        Toast.makeText(getActivity(), string, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+       /*Добавляем метод, который берёт данные из ресурсов меню
+       и преобразует их в пункты меню на экране.
+       По английски "inflate" переводится как надувать,
+       т.е. по замыслу разработчиков Android, мы как бы надуваем данными объект, например, меню.
+       Но на самом деле слово "inflate" происходит от словосочетания in flat - в квартиру.
+       Существует старинная традиция запускать в квартиру первым кота,
+       который исследует все закоулки дома и заявляет о своём согласии жить в нём.
+       Так и мы запускаем данные из XML-файла в объект MenuInflater.
+        */
+        inflater.inflate(R.menu.menu_main, menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // получим идентификатор выбранного пункта меню
+        int id = item.getItemId();
+
+        // Операции для выбранного пункта меню
+        switch (id) {
+            case R.id.action_settings:
+                showMessage("Вы выбрали пункт Настройки");
+
+                return true;
+            case R.id.action_search:
+                showMessage("Вы выбрали Поиск");
+
+                Uri uri = Uri.parse("http://google.com/search?q=haruhi");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+
+                return true;
+            case R.id.action_exit:
+                showMessage("Вы выбрали Выход");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    View view=inflater.inflate(R.layout.fr_main, container, false);
+        return view;
+    }
+
+}
